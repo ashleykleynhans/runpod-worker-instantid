32b4dc0002035cd9164074c1127e23351e0da61c
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2026-02-24 15:28:30 +0200

    Bug fix for venv not being found on Network Volume

--- start_network_volume.sh
+++ start_network_volume.sh
@@ -7,18 +7,17 @@ if [[ ! -L /workspace ]]; then
   ln -s /runpod-volume /workspace
 fi
 
-if [ -f "/workspace/venv/bin/activate" ]; then
+if [ -f "/workspace/venv/bin/python3" ]; then
     echo "Starting RunPod Handler"
-    source /workspace/venv/bin/activate
     TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
     export LD_PRELOAD="${TCMALLOC}"
     export PYTHONUNBUFFERED=1
     export HUGGINGFACE_HUB_CACHE="/runpod-volume/huggingface-cache/hub"
     export TRANSFORMERS_CACHE="/runpod-volume/huggingface-cache/hub"
     cd /workspace/runpod-worker-instantid/src
-    python3 -u handler.py
+    /workspace/venv/bin/python3 -u handler.py
 else
-    echo "ERROR: The Python Virtual Environment (/workspace/venv/bin/activate) could not be activated"
+    echo "ERROR: The Python Virtual Environment (/workspace/venv) could not be found"
     echo "1. Ensure that you have followed the instructions at: https://github.com/ashleykleynhans/runpod-worker-instantid/blob/main/docs/building/with-network-volume.md"
     echo "2. Ensure that you have used the Pytorch image for the installation and NOT an InstantID image."
     echo "3. Ensure that you have attached your Network Volume to your endpoint."
